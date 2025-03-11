1614667f98afb5e9ccd6c219f9cd412eca7bc209
HimaSaiCharan <127401464+HimaSaiCharan@users.noreply.github.com>
2025-03-11 23:39:50 +0530

    changed some css and added eventlistener

--- main.js
+++ main.js
@@ -1,34 +1,58 @@
+const debug = (args) => {
+  console.log(args);
+  return args;
+};
+
 class Game {
   #player1;
   #player2;
   #board;
-  #playerMark;
+  #currentSymbol;
 
   constructor(player1, player2) {
     this.#player1 = player1;
     this.#player2 = player2;
-
-    this.#board = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""],
-    ];
-    this.#playerMark = "X";
+    this.#board = ["", "", "", "", "", "", "", "", ""];
+    this.#currentSymbol = "X";
   }
 
   display() {
     console.log(
-      this.#player1,
-      this.#player2,
-      this.#playerMark,
+      this.#currentSymbol,
       this.#board
     );
+  }
+
+  updateBoard(id) {
+    this.#board[id] = this.#currentSymbol;
+  }
+
+  isCellOccupied(id) {
+    return this.#board[id] !== "";
+  }
+
+  toggleSymbol() {
+    this.#currentSymbol = this.#currentSymbol === "X" ? "O" : "X";
+  }
+
+  get currentSymbol() {
+    return this.#currentSymbol;
   }
 }
 
 const runGame = () => {
   const game = new Game("Me", "You");
-  game.display();
+  const board = document.querySelector(".board");
+
+  board.addEventListener("click", (e) => {
+    const id = parseInt(e.target.id);
+
+    if (!game.isCellOccupied(id) && e.target.className === "cell") {
+      e.target.innerText = game.currentSymbol;
+      game.updateBoard(id);
+      game.toggleSymbol();
+    }
+  });
 };
 
 window.onload = runGame;
