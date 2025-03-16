b00d8a658aae71308fe317df0c959277d9732d9f
HimaSaiCharan <127401464+HimaSaiCharan@users.noreply.github.com>
2025-03-17 00:18:08 +0530

    changed the method of removing the font in the cells

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const debug = (args) => {
-  console.log(args);
-  return args;
-};
-
 class Game {
   #board;
   #currentSymbol;
+  #winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
 
   constructor() {
-    this.#board = ["", "", "", "", "", "", "", "", ""];
+    this.#board = Array(9).fill("");
     this.#currentSymbol = "X";
   }
 
@@ -29,21 +29,42 @@ class Game {
   }
 
   get isWon() {
-    const winPatterns = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8],
-      [0, 3, 6], [1, 4, 7], [2, 5, 8],
-      [0, 4, 8], [2, 4, 6]
-    ];
-
-    return winPatterns.some((pattern) => {
-      return this.#board[pattern[0]] !== "" && pattern.every(pos => this.#board[pos] === this.#board[pattern[0]]);
+    return this.#winPatterns.some((pattern) => {
+      return pattern.every(pos => this.#board[pos] === this.#currentSymbol);
     });
   }
 
   get isDraw() {
     return this.#board.every(cell => cell !== "");
   }
+
+  resetBoard() {
+    this.#board = Array(9).fill("");
+  }
 }
+
+const winnerMsg = (winner) => console.log(`Player ${winner} Won the game`);
+
+const removeFont = (board) => {
+  const fontDisappear = [
+    { fontSize: "36px", opacity: 1 },
+    { fontSize: "0px", opacity: 0 },
+  ];
+
+  const fontDisappearTiming = {
+    duration: 1000,
+    iterations: 1,
+    easing: "ease-in",
+  };
+
+  [...board.children].forEach(child => {
+    child.animate(fontDisappear, fontDisappearTiming);
+
+    setTimeout(() => {
+      child.textContent = "";
+    }, 900);
+  });
+};
 
 const runGame = () => {
   const game = new Game();
@@ -52,23 +73,28 @@ const runGame = () => {
   const handleMove = (e) => {
     const id = parseInt(e.target.id);
 
-    if (!game.isCellOccupied(id) && e.target.className === "cell") {
-      e.target.innerText = game.currentSymbol;
-      game.updateBoard(id);
+    if (game.isCellOccupied(id) || !e.target.matches(".cell")) return;
 
-      if (game.isWon) {
-        console.log("Match Won");
-        console.log(`Player ${game.currentSymbol} Won The Game`);
-        board.removeEventListener("click", handleMove);
-      }
+    e.target.innerText = game.currentSymbol;
+    game.updateBoard(id);
 
-      if (game.isDraw) {
-        console.log("Game completed...");
-      }
+    if (game.isWon) {
+      winnerMsg(game.currentSymbol);
 
-      game.toggleSymbol();
+      board.removeEventListener("click", handleMove);
+      return;
     }
 
+    if (game.isDraw) {
+      removeFont(board);
+
+      game.resetBoard();
+      console.log("Game Draw...");
+
+      return;
+    }
+
+    game.toggleSymbol();
   };
 
   board.addEventListener("click", handleMove);
